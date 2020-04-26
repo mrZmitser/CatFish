e81ce3e3ac6c14e88da121eba951a5aabd0f47a4
Pestonische <61064359+Pestonische@users.noreply.github.com>
2020-04-26 22:30:44 +0300

    0.0.1.2

---
+++ Assets/Scenes/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Работа с интерфейсами
+using UnityEngine.SceneManagement; //Работа со сценами
+using UnityEngine.Audio; //Работа с аудио
+public class MainMenuController : MonoBehaviour
+{
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+           
+            SceneManager.LoadScene("Menu");
+            
+            Time.timeScale = 0;
+        }
+else
+{
+
+ Time.timeScale = 1;
+}
+	}
+}
---
+++ Assets/Scenes/Settings.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Работа с интерфейсами
+using UnityEngine.SceneManagement; //Работа со сценами
+using UnityEngine.Audio; //Работа с аудио
+
+
+public class Settings : MonoBehaviour
+{
+public bool isFullScreen;
+    public void FullScreenToggle()
+    {
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+public AudioMixer am;
+public void AudioVolume(float sliderValue)
+    {
+        am.SetFloat("masterVolume", sliderValue);
+    }
+public void Quality(int q)
+    {
+        QualitySettings.SetQualityLevel(q);
+    }
+Resolution[] rsl;
+List<string> resolutions;
+public Dropdown dropdown;
+public void Awake()
+    {
+        resolutions = new List<string>();
+        rsl = Screen.resolutions;
+        foreach (var i in rsl)
+        {
+            resolutions.Add(i.width +"x" + i.height);
+        }
+        dropdown.ClearOptions();
+        dropdown.AddOptions(resolutions);
+    }
+public void Resolution(int r)
+    {
+        Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);
+    }
+}
+
